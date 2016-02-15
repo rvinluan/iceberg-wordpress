0dbf242213bc0f99217d468f8c384f4ded194650
Robert Vinluan <robert.vinluan@nytimes.com>
2016-02-15 03:26:59 -0500

    fixed a link type bug

--- template-parts/content.php
+++ template-parts/content.php
@@ -27,7 +27,7 @@
 	    <h3><?php echo get_post_custom_values("subtitle")[0]; ?></h3>
 	</div>
 	<div class="link-title">
-			<?php if (get_post_custom_values('link embed')[0] != null) : ?>
+			<?php if (get_post_custom_values('link type')[0] != null) : ?>
 		    <div class="section-label"><?php echo get_post_custom_values("link type")[0]; ?></div>
 		  <?php else : ?>
 		  	<div class="section-label">nothing</div>
