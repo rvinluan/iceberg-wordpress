344ff6ab334f961fd167cafe0102b1a43217d7d8
Robert Vinluan <robert.vinluan@nytimes.com>
2016-03-06 01:14:30 -0500

    use advanced custom fields

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -6,7 +6,11 @@
  *
  * @package iceberg
  */
-
+$subtitle = get_field("subtitle");
+$link_type = get_field("link_type") == "Other" ? get_field("link_type_text") : get_field("link_type");
+$link_title = get_field("link_title");
+$link_url = get_field("link_url");
+$embed_type = get_field("embed_type");
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -17,29 +21,29 @@
       ?>
   </div>
   <div class="subtitle">
-      <h3><?php echo get_post_custom_values("subtitle")[0]; ?></h3>
+      <h3><?php echo $subtitle ?></h3>
   </div>
   <div class="link-title">
-      <?php if (get_post_custom_values('link type')[0] != null) : ?>
-        <div class="section-label"><?php echo get_post_custom_values("link type")[0]; ?></div>
+      <?php if ($link_type != null) : ?>
+        <div class="section-label"><?php echo $link_type ?></div>
       <?php else : ?>
         <div class="section-label">nothing</div>
       <?php endif; ?>
       <div class="link-title-content">
-        <a class="link-title-link" href="<?php echo get_post_custom_values('link url')[0]; ?>"><?php echo get_post_custom_values("link title")[0]; ?></a>
+      <a class="link-title-link" href="<?php echo $link_url ?>"><?php echo $link_title ?></a>
         <div class="embed">
-          <?php if (get_post_custom_values('link embed')[0] != null) : ?>
-            <?php echo get_post_custom_values("link embed")[0]; ?>
-          <?php elseif (get_post_custom_values('link image url')[0] != null) : ?>
-            <a href="<?php echo get_post_custom_values('link url')[0]; ?>">
-            <img src='<?php echo get_post_custom_values("link image url")[0]; ?>'>
+          <?php if ($embed_type == "Embed") : ?>
+            <?php echo get_field("embed_embed_code") ?>
+          <?php elseif ($embed_type == "Image") : ?>
+            <a href="<?php echo $link_url ?>">
+            <img src='<?php echo get_field("embed_image_url") ?>'>
             </a>
-          <?php elseif (get_post_custom_values('link quote')[0] != null) : ?>
-            <a href="<?php echo get_post_custom_values('link url')[0]; ?>">
-            <blockquote>“<?php echo get_post_custom_values("link quote")[0]; ?>”</blockquote>
+          <?php elseif ($embed_type == "Quote") : ?>
+            <a href="<?php echo $link_url ?>">
+            <blockquote>“<?php echo get_field("embed_quote_text") ?>”</blockquote>
             </a>
           <?php else : ?>
-            <a class="generic-link-out" href="<?php echo get_post_custom_values('link url')[0]; ?>">
+            <a class="generic-link-out" href="<?php echo $link_url ?>">
               <div>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 87.5"><path d="M59.3 12.8c-.8-.8-2-1.3-3.3-1.3H30c-1.3 0-2.5.5-3.3 1.3-.8.8-1.4 2-1.4 3.2v22.3c0 1.2.5 2.4 1.4 3.2.8.8 2 1.3 3.3 1.3h7v-3.9h-7.6V15.4h27.3V39H49v3.9h7c1.3 0 2.5-.5 3.3-1.3.8-.8 1.4-2 1.4-3.2V16c0-1.2-.5-2.4-1.4-3.2z"/><path d="M43.6 28.5c-.8-.8-2-1.3-3.3-1.3h-6.9v3.9H41v23.6H13.7V31.1h7.7v-3.9h-7c-1.3 0-2.5.5-3.3 1.3-.8.8-1.4 2-1.4 3.2V54c0 1.2.5 2.4 1.4 3.2.8.8 2 1.3 3.3 1.3h25.9c1.3 0 2.5-.5 3.3-1.3.8-.8 1.4-2 1.4-3.2V31.7c0-1.2-.5-2.4-1.4-3.2z"/></svg>
               ⟶
